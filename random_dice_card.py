ee85abbf20fe861a83e005618fa6bb43904573b5
Mayuri Manojkumar Mhetre <68188457+mayurimhetre@users.noreply.github.com>
2021-05-13 17:01:24 +0530

    Create random_dice_card.py
    Function Used : random.choices" 
    to select random value from the values given in list for Dice example and deck of card example.
    Module Used : random

---
+++ random_dice_card.py
@@ -0,0 +1,25 @@
+####### Select a crad from Deck of crads #####
+import random
+cards = ["Diamonds", "Spades", "Hearts", "Clubs"]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+
+def pick_a_card():
+    card = random.choices(cards)
+    rank = random.choices(ranks)
+    print(card[0])
+    print(rank[0])
+    print("We have selected '{}' of '{}' ".format(rank[0],card[0]))
+    
+
+pick_a_card()
+
+
+######## Roll a dice ########################
+import random
+
+dice = [1,2,3,4,5,6]
+
+def  dice_select():
+    dice1 = random.choices(dice)
+    print("Value on dice after rolling:{}".format(dice1[0]))
+dice_select()
